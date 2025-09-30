dcfbd744103360bc063b85c12881de86a062ea8a
kh4f <kh4f.dev@gmail.com>
2025-09-30 10:24:36 +0300

    chore(debug): configure debuggin directly from VS Code
    - Add debug launch profiles to `launch.json`
    - Update `tsdown.config.ts` to add source maps with transformed paths

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'tsdown'
+import { pathToFileURL } from 'node:url'
 
 const isProd = process.argv.includes('--prod')
+const dirUrl = pathToFileURL(import.meta.dirname).href
 
 export default defineConfig({
 	entry: 'src/plugin.ts',
 	outputOptions: {
 		entryFileNames: 'main.js',
 		minify: isProd,
+		sourcemapBaseUrl: dirUrl,
+		sourcemapPathTransform: relativeSourcePath =>
+			`${dirUrl}/${relativeSourcePath}`,
 	},
+	sourcemap: !isProd,
 	format: 'cjs',
 	outDir: '.',
 	clean: false,
